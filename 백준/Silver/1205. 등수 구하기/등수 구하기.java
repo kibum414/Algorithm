c304ce440b5bf2dce01fb057a1dcc5ab5c29865b
박기범 <76759852+kibum414@users.noreply.github.com>
2022-05-09 00:08:43 +0900

    [Silver IV] Title: 등수 구하기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,23 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, S, P;
     static Integer[] scores;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = scanner.nextInt();
-        S = scanner.nextInt();
-        P = scanner.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
         scores = new Integer[N];
 
+        if (N != 0) st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            scores[i] = scanner.nextInt();
+            scores[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(scores, Collections.reverseOrder());
