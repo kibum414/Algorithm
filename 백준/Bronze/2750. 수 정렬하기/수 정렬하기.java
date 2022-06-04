edff173086a7b2ecaabad63995fa7f25fd6b96c3
박기범 <76759852+kibum414@users.noreply.github.com>
2022-06-05 00:01:51 +0900

    [Bronze I] Title: 수 정렬하기, Time: 136 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
@@ -19,27 +20,13 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        selectionSort();
+        Arrays.sort(arr);
 
         for (int i = 0; i < N; i++) {
             sb.append(arr[i]).append("\n");
         }
 
         System.out.println(sb);
-
-    }
-
-    private static void selectionSort() {
-
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
 
     }
 
