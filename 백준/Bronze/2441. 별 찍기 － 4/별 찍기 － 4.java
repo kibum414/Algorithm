a54bc9c5b73a30127eda9c323c6c49332cf5954e
박기범 <76759852+kibum414@users.noreply.github.com>
2022-06-23 23:35:12 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 0, len = N - i; j < len; j++) {
+                sb.append(" ");
+            }
+
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
