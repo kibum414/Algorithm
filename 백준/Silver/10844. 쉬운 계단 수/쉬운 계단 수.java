3316dde4ad5f9051d4cd0922ffb92388e9826547
박기범 <76759852+kibum414@users.noreply.github.com>
2022-08-16 00:18:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 136 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static Long[][] dp;
+    static long result;
+    final static long MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new Long[N + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1L;
+        }
+
+        result = 0;
+
+        for (int i = 1; i < 10; i++) {
+            result += recursive(N, i);
+        }
+
+        System.out.println(result % MOD);
+
+    }
+
+    private static long recursive(int n, int value) {
+
+        if (n == 1) return dp[n][value];
+
+        if (dp[n][value] == null) {
+            if (value == 0) dp[n][value] = recursive(n - 1, 1);
+            else if (value == 9) dp[n][value] = recursive(n - 1, 8);
+            else dp[n][value] = recursive(n - 1, value - 1) + recursive(n - 1, value + 1);
+        }
+
+        return dp[n][value] % MOD;
+
+    }
+
+}
