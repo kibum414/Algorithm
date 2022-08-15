d4299dd55094f0a4c9929c28d2c7a1ee7717406b
박기범 <76759852+kibum414@users.noreply.github.com>
2022-08-16 02:01:22 +0900

    [Silver I] Title: 귀여운 라이언, Time: 440 ms, Memory: 73724 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K, answer;
+    static int N, K, end, cnt, answer;
     static int[] arr;
-    static ArrayList<Integer> list;
 
     public static void main(String[] args) throws IOException {
 
@@ -17,40 +15,28 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        list = new ArrayList<>();
+        arr = new int[N + 1];
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-
-            if (arr[i] == 1) list.add(i);
         }
 
-        if (list.size() < K) {
-            System.out.println(-1);
+        end = 0;
+        answer = N + 1;
+        cnt = 0;
 
-            return;
-        }
+        for (int start = 1; start <= N; start++) {
+            if (arr[start - 1] == 1) cnt--;
 
-        answer = Integer.MAX_VALUE;
-
-        for (int start = 0, len = list.size(); start < len; start++) {
-            int end = start;
-            int cnt = 0;
-
-            while (end < len && cnt < K) {
-                end++;
-                cnt++;
+            while (end + 1 <= N && cnt < K) {
+                if (arr[++end] == 1) cnt++;
             }
 
-            if (cnt == K) {
-                int left = list.get(start);
-                int right = list.get(end - 1);
-
-                answer = Math.min(answer, right - left + 1);
-            }
+            if (cnt == K) answer = Math.min(answer, end - start + 1);
         }
+
+        if (answer == N + 1) answer = -1;
 
         System.out.println(answer);
 
