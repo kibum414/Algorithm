55a714c682a6994babe9d5827396ef280ea25988
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-09-04 02:52:40 +0900

    [Bronze IV] Title: 기찍 N, Time: 216 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] 기찍 N - 2742 
+
+[문제 링크](https://www.acmicpc.net/problem/2742) 
+
+### 성능 요약
+
+메모리: 18152 KB, 시간: 216 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
