f3f323e599489302b9136f6593bdcd67cfca4505
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-09-23 01:15:50 +0900

    [Bronze I] Title: 더하기 사이클, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N, cnt, cur, tens, units;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        cur = N;
+        cnt = 0;
+
+        do {
+            tens = cur / 10;
+            units = cur % 10;
+
+            int temp = (tens + units) % 10;
+
+            cur = units * 10 + temp;
+
+            cnt++;
+        } while (N != cur);
+
+        System.out.println(cnt);
+
+    }
+
+}
