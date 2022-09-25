40d5f9477973e75e6b48ebdd7108edb10d6c5db1
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-09-25 19:54:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 212 ms, Memory: 17032 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,16 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+
+    static int str, pre_str, cnt;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(st.countTokens());
+        cnt = 0;
+        pre_str = 32;
+
+        while (true) {
+            str = br.read();
+
+            if (str == 32) {
+                if (pre_str != 32) cnt++;
+            } else if (str == 10) {
+                if (pre_str != 32) cnt++;
+
+                break;
+            }
+
+            pre_str = str;
+        }
+
+        System.out.println(cnt);
 
     }
 
