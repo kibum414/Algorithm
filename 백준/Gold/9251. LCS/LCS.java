20114dff65db66d26a171b7bf6468557a41027e0
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-10-08 15:24:35 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static String str1, str2;
+    static int len1, len2;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine();
+        str2 = br.readLine();
+        len1 = str1.length();
+        len2 = str2.length();
+
+        dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println(dp[len1][len2]);
+
+    }
+
+}
