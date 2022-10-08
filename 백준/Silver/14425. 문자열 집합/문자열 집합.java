97906b23aa99d36c5c327b2179dd7480df44c676
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-10-08 22:50:43 +0900

    [Silver III] Title: 문자열 집합, Time: 408 ms, Memory: 31216 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, cnt;
+    static Set<String> set;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        cnt = 0;
+        set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (set.contains(br.readLine())) cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+
+}
