a696cae141bd5819f93b4786fdacf381f5f34448
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-10-24 23:24:12 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,21 +1,18 @@
 class Solution {
     
     public boolean solution(String s) {
-        
-        boolean answer = true;
-        int len = s.length();
-        
-        if (len != 4 && len != 6) return false;
-        
-        for (int i = 0; i < len; i++) {
-            if (s.charAt(i) >= 48 && s.charAt(i) <= 57) continue;
 
-            answer = false;
-            
-            break;
+        int len = s.length();
+
+        if (len != 4 && len != 6) return false;
+
+        try {
+            int n = Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
         }
         
-        return answer;
+        return true;
         
     }
 }
