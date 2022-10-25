4f9378ebf49e5cc3be58b20da9f29a627d552c48
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-10-25 16:50:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.04 ms
+메모리: 77.4 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,18 +1,17 @@
 class Solution {
-    
+
     public boolean solution(String s) {
 
         int len = s.length();
 
         if (len != 4 && len != 6) return false;
 
-        try {
-            int n = Integer.parseInt(s);
-        } catch (Exception e) {
-            return false;
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
         }
-        
+
         return true;
-        
+
     }
+
 }
