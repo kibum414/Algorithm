e4dcddc790c4d8e80ed9fc6ec76ad16c265c842a
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-11-01 17:07:39 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+
     static String[] W;
     static boolean[] visit;
     static int len, answer;
@@ -13,34 +13,42 @@ class Solution {
 
         dfs(begin, target, 0);
 
+        // 변환할 수 없는 경우 0 반환
         if (answer == 51) return 0;
 
         return answer;
 
     }
-    
+
     private static void dfs(String begin, String target, int cnt) {
 
+        // 단어가 target 과 같아졌을 때 횟수 저장
         if (begin.equals(target)) {
-            answer = cnt;
+            answer = Math.min(answer, cnt);
 
             return;
         }
 
         for (int i = 0; i < len; i++) {
+            // 변환한 적 없으면서 알파벳이 하나만 다른 경우
             if (!visit[i] && check(begin, W[i])) {
+                // 찾은 단어 표시
                 visit[i] = true;
+                // cnt 증가시키면서 다음 단계로
                 dfs(W[i], target, cnt + 1);
+                // 완전 탐색을 위해 표시 제거
                 visit[i] = false;
             }
         }
 
     }
 
+    // 두 단어 비교
     private static boolean check(String s1, String s2) {
 
         int cnt = 0;
 
+        // 두 단어를 비교하며 알파벳이 두 개 이상 다른 경우 false 리턴
         for (int i = 0, sLen = s1.length(); i < sLen; i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 if (++cnt > 1) return false;
