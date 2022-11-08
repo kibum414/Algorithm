39751e5fd08365a61b0a679e6175e16f4aeeac62
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-11-08 23:01:54 +0900

    [Bronze III] Title: 윷놀이, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int sum;
+    static char[] arr = {'D', 'C', 'B', 'A', 'E'};
 
     public static void main(String[] args) throws IOException {
 
@@ -15,17 +15,13 @@ public class Main {
 
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
-            sum = 0;
+            int sum = 0;
 
             for (int j = 0; j < 4; j++) {
                 sum += Integer.parseInt(st.nextToken());
             }
 
-            if (sum == 3) sb.append("A").append("\n");
-            else if (sum == 2) sb.append("B").append("\n");
-            else if (sum == 1) sb.append("C").append("\n");
-            else if (sum == 0) sb.append("D").append("\n");
-            else sb.append("E").append("\n");
+            sb.append(arr[sum]).append("\n");
         }
 
         System.out.println(sb);
