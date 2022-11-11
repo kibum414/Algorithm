3c0654ff141583785360c49d408496ccd86ee214
Ki Beom Park <76759852+kibum414@users.noreply.github.com>
2022-11-11 19:04:57 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N, max;
+    static int N, cnt;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -13,24 +13,28 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         arr = new int[10];
-        max = -1;
+        cnt = 1;
+
+        if (N < 10) {
+            System.out.println(1);
+
+            return;
+        }
 
         while (N > 0) {
-            int n = N % 10;
-
-            if (n == 6 || n == 9) {
-                if (arr[n] == max && max > arr[15 - n]) arr[15 - n]++;
-                else arr[n]++;
-            } else {
-                arr[n]++;
-            }
-
-            if (max < arr[n]) max = arr[n];
-
+            arr[N % 10]++;
             N /= 10;
         }
 
-        System.out.println(max);
+        for (int i = 0; i < 10; i++) {
+            if (i == 6 || i == 9) continue;
+
+            cnt = Math.max(cnt, arr[i]);
+        }
+
+        cnt = Math.max(cnt, (arr[6] + arr[9] + 1) / 2);
+
+        System.out.println(cnt);
 
     }
 
