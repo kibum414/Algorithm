956239d1d1fdc27fee6825eee0e54456c9045735
KiBeom Park <76759852+kibum414@users.noreply.github.com>
2022-12-19 16:49:55 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 7.39 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -4,45 +4,44 @@ class Solution {
 
     public int solution(int[] queue1, int[] queue2) {
 
-        Queue<Integer> q1 = new ArrayDeque<>();
-        Queue<Integer> q2 = new ArrayDeque<>();
-
-        int len = queue1.length;
-        long s1 = 0, s2 = 0;
+        int len = queue1.length, size = len * 2;
+        int[] queue = new int[size];
+        long sum1 = 0, sum2 = 0;
 
         for (int i = 0; i < len; i++) {
-            int n1 = queue1[i], n2 = queue2[i];
+            int n1 = queue1[i];
+            int n2 = queue2[i];
 
-            q1.add(n1);
-            s1 += n1;
+            queue[i] = n1;
+            queue[len + i] = n2;
 
-            q2.add(n2);
-            s2 += n2;
+            sum1 += n1;
+            sum2 += n2;
         }
 
-        if ((s1 + s2) % 2 != 0) return -1;
+        if ((sum1 + sum2) % 2 != 0) return -1;
 
-        int cnt = 0, n = 0;
+        int answer = 0, idx1 = 0, idx2 = len;
 
-        while (s1 != s2) {
-            if (s1 > s2) {
-                n = q1.poll();
-                
-                q2.add(n);
-                s1 -= n;
-                s2 += n;
+        while (sum1 != sum2) {
+            if (sum1 > sum2) {
+                sum1 -= queue[idx1];
+                sum2 += queue[idx1];
+
+                idx1++;
             } else {
-                n = q2.poll();
-                
-                q1.add(n);
-                s1 += n;
-                s2 -= n;
+                sum1 += queue[idx2];
+                sum2 -= queue[idx2];
+
+                idx2++;
             }
 
-            if (++cnt > len * 3) return -1;
+            answer++;
+
+            if (idx1 >= size || idx2 >= size) return -1;
         }
 
-        return cnt;
+        return answer;
 
     }
 
