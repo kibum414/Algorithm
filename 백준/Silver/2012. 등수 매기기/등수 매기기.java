0e210dfadbfdf4eb1adad98cf3f4dec0e39571ac
KiBeom Park <76759852+kibum414@users.noreply.github.com>
2023-01-18 23:21:59 +0900

    [Silver III] Title: 등수 매기기, Time: 820 ms, Memory: 52612 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            answer += Math.abs(i - arr[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+
+}
